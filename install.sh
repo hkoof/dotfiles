15b266bca7eade49c236d7709a0ef3b5c6d7613d
Heiko Noordhof <heiko@vcs.service.rug.nl>
2013-06-25 12:16:38 +0200

    install script added

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+SRCS="
+  .Xresources
+  .screenrc
+  .config
+  .emacs
+  .vimrc
+  .vimbackup
+  .vim
+"
+
+cp -rv $SRCS ~/
+
+# git-config  special treatment: preserve user info
+mv ~/.gitconfig ~/.gitconfig.bak 2>/dev/null
+sed '/^\[user\]/,/^\[/!d' ~/.gitconfig.bak | sed '$d' >~/.gitconfig
+cat .gitconfig >>~/.gitconfig
+

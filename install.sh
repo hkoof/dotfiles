3d004b215eeeb5cd20d95a6844302faf33858d35
Heiko Noordhof <h.e.noordhof@rug.nl>
2013-12-11 11:07:34 +0100

    fix minor bug in install script

--- install.sh
+++ install.sh
@@ -14,7 +14,6 @@ SRCS="
 cp -rv $SRCS ~/
 
 # git-config  special treatment: preserve user info
-mv ~/.gitconfig ~/.gitconfig.bak 2>/dev/null
-sed '/^\[user\]/,/^\[/!d' ~/.gitconfig.bak | sed '$d' >~/.gitconfig
+mv ~/.gitconfig ~/.gitconfig.bak 2>/dev/null && sed '/^\[user\]/,/^\[/!d' ~/.gitconfig.bak | sed '$d' >~/.gitconfig
 cat .gitconfig >>~/.gitconfig
 

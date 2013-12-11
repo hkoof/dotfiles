d526d30425f92e686053d127474ace6bdb5d3558
Heiko Noordhof <h.e.noordhof@rug.nl>
2013-12-11 14:53:04 +0100

    bash promt only to show basedir

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ if ! grep -q GIT_PS1 ~/.bashrc ; then # crude heuristics for idempotency
     echo >>~/.bashrc
     cat <<\EOF >>~/.bashrc
 source ~/bin/git-prompt.sh
-export PS1='\u@\h:\w$(__git_ps1 "[%s]")\$ '
+export PS1='\u@\h:\W$(__git_ps1 "[%s]")\$ '
 GIT_PS1_SHOWSTASHSTATE=1
 GIT_PS1_SHOWUNTRACKEDFILES=1
 #GIT_PS1_SHOWUPSTREAM="auto"

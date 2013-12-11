235c71e4e337c42d48b29c04f94617d025da613a
Heiko Noordhof <h.e.noordhof@rug.nl>
2013-12-11 14:23:47 +0100

    add git-aware bash prompt

--- install.sh
+++ install.sh
@@ -17,3 +17,16 @@ cp -rv $SRCS ~/
 mv ~/.gitconfig ~/.gitconfig.bak 2>/dev/null && sed '/^\[user\]/,/^\[/!d' ~/.gitconfig.bak | sed '$d' >~/.gitconfig
 cat .gitconfig >>~/.gitconfig
 
+# git-aware bash prompt
+if ! grep -q GIT_PS1 ~/.bashrc ; then # crude heuristics for idempotency
+    echo >>~/.bashrc
+    cat <<\EOF >>~/.bashrc
+source ~/bin/git-prompt.sh
+export PS1='\u@\h:\w$(__git_ps1 "[%s]")\$ '
+GIT_PS1_SHOWSTASHSTATE=1
+GIT_PS1_SHOWUNTRACKEDFILES=1
+#GIT_PS1_SHOWUPSTREAM="auto"
+GIT_PS1_SHOWUPSTREAM="git"
+EOF
+fi
+
